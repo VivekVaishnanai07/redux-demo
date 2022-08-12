0cbb6157b69fa8ce848a0916773b9aa4d5023248
vivek4243 <91829247+vivek4243@users.noreply.github.com>
2022-08-12 13:01:09 +0530

    add qty option

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter, Route, Routes
+} from "react-router-dom";
 import "./App.css";
+import { connect } from "react-redux";
 import CartView from "./components/cart-view/cart-view";
+import ProductView from "./components/product-view/product-view";
 import Header from "./components/header/header";
-import ProductsView from "./components/products-view/products-view";
 
-function App() {
+function App({ current }) {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<ProductsView />} exact />
-        <Route path='/cart-view' element={<CartView />} exact />
-      </Routes>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<ProductView />} />
+          <Route exact path="/cart-view" element={<CartView />} />
+        </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    current: state.shop.currentItem,
+  };
+};
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { Provider } from "react-redux";
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import store from './components/redux/store';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
